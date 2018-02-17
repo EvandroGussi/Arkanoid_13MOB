a2544231c354ffa4316daea4359bde44c42be2d8
EvandroGussi <32003904+EvandroGussi@users.noreply.github.com>
2018-02-17 20:22:32 -0200

    First Commit

---
+++ Assets/Scripts/FinalTelaScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalTelaScript : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		SceneManager.LoadScene ("Intro");
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float velocidade;
+
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * h * velocidade;
+
+
+
+	}
+}
